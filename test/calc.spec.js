7b04319802bf5f6efdae6d8706f30bf2dda0b5e6
Etty Sekhon <etty.sekhon@nexgeneration-solutions.com>
2015-05-21 18:36:51 +0100

    use local mocha

---
+++ test/calc.spec.js
@@ -0,0 +1,12 @@
+var calc = require('../lib/calc');
+var assert = require('assert');
+
+describe('calc', function () {
+
+
+    it('adds 2 numbers', function () {
+        var result = calc.add(1, 2);
+        assert(result === 3, 'Add not working');
+    });
+
+});
