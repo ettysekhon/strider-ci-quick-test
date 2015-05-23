0b51f4a608315a643c410778db9389ecfd1028dd
Etty Sekhon <etty.sekhon@nexgeneration-solutions.com>
2015-05-23 21:51:22 +0100

    fix test

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -6,7 +6,7 @@ describe('calc', function () {
 
     it('adds 2 numbers', function () {
         var result = calc.add(1, 3);
-        assert(result === 3, 'Add not working');
+        assert(result === 4, 'Add not working');
     });
 
 });
